45bcb96f71c07534c18eb870e47c7ae861e643eb
Archimedes Fagundes Junior <archimedes.fagundes@gmail.com>
2022-05-24 10:08:41 -0300

    Split app.js in modules

--- app.js
+++ app.js
@@ -1,45 +1,13 @@
 #!/usr/bin/env node
 
-const stream = require('youtube-audio-stream');
-const fs = require('fs');
+const parseArguments = require('./lib/argument-parser');
+const youtubeStreamToFile = require('./lib/youtube-stream');
 
-const args = process.argv.slice(2);
+const args = parseArguments(process.argv.slice(2));
 
-if (args.length == 0) {
-    console.log("usage: app.js <YOUTUBE URL> [-o <OUTPUT PATH.mp3>]");
+if (args.errorMessage) {
+    console.error(args.errorMessage);
     process.exit(1);
 }
 
-const getOutputPath = (args) => {
-    if (args.length == 1) return '/tmp/audio.mp3';
-
-    if (args[1] != '-o') {
-        console.log("Second parameter must be '-o'");
-        process.exit(1);
-    }
-
-    if (!args[2]) {
-        console.log("Output path must be informed");
-        process.exit(1);
-    }
-
-    return args[2];
-}
-
-const videoUri = args[0];
-const output = getOutputPath(args);
-
-const downloadStream = async () => {
-    console.log(`Extracting audio from ${videoUri}`);
-
-    const writeStream = fs.createWriteStream(output);
-    writeStream.on('finish', () => console.log(`File saved to ${output}`));
-
-    for await (chunk of stream(videoUri)) {
-        writeStream.write(chunk);
-    }
-
-    writeStream.end();
-}
-
-downloadStream();
+youtubeStreamToFile(args);
